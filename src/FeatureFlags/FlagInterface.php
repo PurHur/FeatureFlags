87560a3607333ac0645bb5ef1fc127e652dae850
PurHur <purhur@purh.pw>
2019-05-10 12:07:27 +0200

    Fixed with right php version

--- src/FeatureFlags/FlagInterface.php
+++ src/FeatureFlags/FlagInterface.php
@@ -4,5 +4,20 @@ namespace FeatureFlags;
 
 interface FlagInterface
 {
+    /**
+     * @param string $group
+     * @return bool
+     */
+    public function inGroup(string $group): bool;
 
+
+    /**
+     * @return bool
+     */
+    public function isActive(): bool;
+
+    /**
+     * @return bool
+     */
+    public function isInactive(): bool;
 }
